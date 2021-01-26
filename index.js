548c05379748c79f32490e0b7deae54e0e7cca63
Arnav Gosain <arnav@arnavgosain.com>
2021-01-26 21:22:33 +0530

    remove unused code
    Signed-off-by: Arnav Gosain <arnav@arnavgosain.com>

--- index.js
+++ index.js
@@ -57,7 +57,8 @@ imap.once("ready", () => {
       f.on("message", function (msg, seqno) {
         msg.on("body", function (stream, info) {
           streamToString(stream).then((data) => {
-            api.quickCapture(msg);
+            const dailyNoteUid = api.dailyNoteUid();
+            api.createBlock(msg, dailyNoteUid);
           });
         });
       });
@@ -65,39 +66,6 @@ imap.once("ready", () => {
         console.log("Done fetching all messages!");
       });
       return null;
-
-      imap.search(["NEW"], function (err, results) {
-        if (err || !results.length) {
-          console.log("yay", err, results);
-          return null;
-        }
-
-        // fetch all resulting messages
-        const f = imap.fetch(results, { bodies: "" });
-        f.on("message", function (msg, seqno) {
-          console.log("Saving message #%d", seqno);
-          let prefix = "(#" + seqno + ") ";
-          msg.on("body", function (stream, info) {
-            streamToString(stream).then((data) => {
-              console.log(data);
-              // api.quickCapture(msg);
-            });
-          });
-          msg.once("attributes", function (attrs) {
-            console.log(prefix + "Attributes: %s", inspect(attrs, false, 8));
-          });
-          msg.once("end", function () {
-            console.log(prefix + "Finished");
-          });
-        });
-        f.once("error", function (err) {
-          console.log("Fetch error: " + err);
-        });
-        f.once("end", function () {
-          console.log("Done fetching all messages!");
-          imap.end();
-        });
-      });
     });
   });
 });
